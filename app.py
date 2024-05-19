da9342ce9a79e1ac0a9f2ff24361f269f369102d
rustamovaaa09 <164532926+rustamovaaa09@users.noreply.github.com>
2024-05-19 19:13:39 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    send(msg, broadcast=True)
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', to=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
